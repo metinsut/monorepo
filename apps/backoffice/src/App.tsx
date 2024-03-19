7caa9fce5f627f72a86eddab8e00a8e4fbb5270f
metinsut <metindir@gmail.com>
2024-03-20 01:53:08 +0300

    first commit

---
+++ apps/backoffice/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@repo/ui/components/ui/button';
+
+function App() {
+  return (
+    <main className="flex p-2">
+      <Button>Click me</Button>
+      <h1 className="text-3xl font-bold underline">Vite</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ apps/backoffice/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss';
+import sharedConfig from '@repo/ui/tailwind.config';
+
+const config: Pick<Config, 'content' | 'presets'> = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  presets: [sharedConfig],
+};
+
+export default config;
