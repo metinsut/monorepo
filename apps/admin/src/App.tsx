ff978e9bb0eefefbc2fae0001a206b6a9646209d
metinsut <metindir@gmail.com>
2024-03-20 22:02:53 +0300

    add next

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,10 +1,9 @@
 import { Button } from '@repo/ui/components/ui/button';
-import { cn } from '@repo/ui/lib/utils';
 
 function App() {
   return (
     <main className="flex p-2">
-      <Button className={cn('bg-destructive')}>Click me</Button>
+      <Button className="bg-destructive">Click me</Button>
       <h1 className="text-3xl font-bold underline">Vite</h1>
     </main>
   );
--- apps/admin/tailwind.config.ts
+++ apps/admin/tailwind.config.ts
@@ -1,11 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss';
 import sharedConfig from '@repo/ui/tailwind.config';
 
-export default {
+const config = {
   presets: [sharedConfig],
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
+
+export default config;
